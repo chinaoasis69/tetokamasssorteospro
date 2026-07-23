784cbd38c0556771ab238d808f4e1fa24b37b3c5
chinaoasis69 <marper.69@gmail.com>
2026-07-23 17:51:42 -0500

    Update mass-id.js

--- mass-id.js
+++ mass-id.js
@@ -12706,8 +12706,9 @@ const {
 } 
 
 /* Cargar Dispositivos conectados */
- async function cargarDispositivosConectadosMassId()
+ async function cargarDispositivosConectadosMassId() {
   console.log("ENTRÓ A CARGAR DISPOSITIVOS CONECTADOS");
+  
   if (!listaDispositivosConectados) {
     return;
   }
